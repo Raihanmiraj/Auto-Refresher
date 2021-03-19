ab9ba22636044c4ece7ad1e71c1da7772473607d
Raihanmiraj <65871430+Raihanmiraj@users.noreply.github.com>
2021-03-19 12:30:46 +0600

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
  autorefresh.onclick = function() {
      var timeforreload = document.getElementById("timeforreload").value;
      setInterval(function(){
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //       chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
-  //   });
-  chrome.tabs.reload();
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+    });
+  // chrome.tabs.reload();
 
      },timeforreload*1000 );
  
